7ac40d1090f7a24f71f06ccd0ca152fc53ff1716
viktorHbenitez <viktorHbenitez@hotmail.com>
2016-04-18 00:36:23 -0500

    Functions for arrays

--- index.php
+++ index.php
@@ -1,55 +1,70 @@
 <?php 
 
-	/*Funciones para cadenas de texto
-		htmlspecialchars(String) = Convertir caracteres especial en entidades html
-	*/
-		$caracteresEspeciales = ' < > & "" ';
-		//  htmlspecialchars Ayuda a que no se realizen injecciones a nuestras paginas web
+	echo "Funciones utiles para arreglos <br>";
 
-		// Convirtiendo los caracteres especiales en elementos del DOM
-		echo htmlspecialchars($caracteresEspeciales);	
-		echo '<br>';
-		echo '<br>';
-		//FUNCION : trim() Elimina los espacios en blanco tanto al inicio como al final
+	//funcion extract() extare los elementos asociativos como si fueran variables
+	//extrae las key como si fueran variables con su rrespectivo valor
+	echo "Funciones: extract() <br>";
+	//declaracion de un arreglo asociativo
+	$amigo = array('telefono' => 2434312033,'edad' => 26,'pais' => 'Mexico');
+	//funcion que convierte las llaves en variables
+	extract($amigo);
+	echo "Telefono: ".$telefono;
+	echo "<br>";
+	echo "Edad: ".$edad;
+	echo "<br>";
+	echo "Pais: ".$pais;
+	echo "<br>";
 
-		$textoTrim = '      Ejemplo de texto     ';
-		
-		echo "Texto original".$textoTrim."sin usarla funcion trim()";
-		echo '<br>';
-		echo '<br>';
-		echo "Texto".trim($textoTrim)."usando la funcion trim()";
+	echo "Funciones utiles para arreglos <br>";
+	echo "<br>";
+	echo "Funcion array_pop() Deletes the last element 	of an array";
+	echo "<br>";
 
-		//strlen() = calcula el tamano de un String
+	//Declaracion del arreglo
+	$diaSemana = array('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo');
+	
+	//Recorremos el arreglo
+	/*for ($i=0; $i < $tamanoArreglo; $i++) { 
+		echo $diaSemana[$i]."<br>";
+	}*/
 
-		$textoString = 'viktor hugo';
-		echo '<br>';
-		echo "La palabra ".$textoString. " 	tiene ".strlen($textoString)." caracteres";
+	//Recorrer con foreach
+	foreach ($diaSemana as $dia) {
+		echo $dia."<br>";
+	}
+	//Deletes the las element of an array
+	//$lastDay = array_pop($diaSemana);
+	$tamanoArreglo = count($diaSemana);
+	echo "<hr>";
+	for($i=0; $i < $tamanoArreglo;$i++){
+		echo $diaSemana[$i]."<br>";
+	}
+	echo "<hr>";
+	//echo $lastDay."<br/>";
 
-		//substr() = regresa un intervalo de caracteres de un String
-		$textoIntervalo = 'viktor hugo';
-		echo '<br>';
-		echo "de la palabra ".$textoIntervalo. "Se va a estraer la palabra viktor=".substr($textoIntervalo,0,6);
-		
-		//strtoupper() strtolower() converti un String en mayusculas y minusculas
-		echo "<br>";
-		echo "<br>";
-		echo 'strtoupper() strtolower() ';
-		echo "<br>";
-		$textOrigin = 'vikToR huGo benItez';
-		echo "El texto original es ".$textOrigin;
-		echo "<br>";
-		echo "Texto con la funcion strtoupper: ".strtoupper($textOrigin);
-		echo "<br>";
-		echo "Texto con la funcion strtolower: ".strtolower($textOrigin);
+	//Funcion join('caracter / String',array); concatena cualquier elemento string o caracter y lo muestra sin  necesidad de un ciclo
+	echo join(' - ',$diaSemana);
+	echo "<br>";
+
+	//funcion count(): Permite conocer cuantos elementos tenemos en nuestro arreglo
+	echo "El arreglo diaSemana tiene ".count($diaSemana)." dias<br/>";
+	echo "<br>";
+
+	echo "Funcion::sort()<br>";
+	//funcion sort(): Permite ordenar nuestro arreglo de forma ascendente
+	/*sort($diaSemana);
+
+	foreach ($diaSemana as $dia) {
+		# code...
+		echo $dia."<br/>";
+	}
+	*///Funcion array_reverse(): Muestra el arreglo de forma invertida
+	$arreglo_reverse = array_reverse($diaSemana);
+	echo join(' , ',$arreglo_reverse);
 
 
-		echo "<br>";
-		//strpos() permite buscar un caracter en un String
-		$textoBuscar = 'Texto en el que se buscara una letra';
-		echo "<br>";
-		echo "<br>";
-		echo 'buscamos la letra s '.strpos($textoBuscar,'s');
-		echo "<br>";
+
 
 
  ?>
