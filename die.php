e3f4cbb2422c282110a9bed1732ef99147c4383b
viktorHbenitez <viktorHbenitez@hotmail.com>
2016-04-27 13:52:36 -0500

    Funcion die() Interrumpe la ejecucon de la pagina HTML al encontrar la funcion die()

---
+++ die.php
@@ -0,0 +1,22 @@
+<?php 
+	/*Funcion die() = para la ejecucion del codigo al encontrar esta funcion
+	Ejemplo: die();
+	*/
+
+	echo "<h1>Titulo dentro del codigo PHP<h1>";
+	/*die(); del codigo al encontra la funcion */
+ ?>
+
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Funcion die()</title>
+ </head>
+ <body>
+ 	<h1> Funcion die() interrumpe la ejecucion <small>del codigo al encontra la funcion</small></h1>
+	<?php /*die(); Interrumpe la ejecucion del codigo  de la pagina*/ ?>
+	<h2>Titulo en el documento HTML</h2>
+ </body>
+ </html>
