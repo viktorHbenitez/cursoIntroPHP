24fbe4f119e193c1a6ca75534338b31ced8a310b
viktorHbenitez <viktorHbenitez@hotmail.com>
2016-04-28 00:27:31 -0500

    Formulario 1.4 Validacion N/A datos desde formulario enviados metodo POST

--- formulario/datosRecibidos.php
+++ formulario/datosRecibidos.php
@@ -1,10 +1,19 @@
 <?php 
 
+	/*METODO POST INFORMACION SENSIBLE YA QUE SE ENVIAN ATRAS DEL NAVEGADOR Y NO POR URL*/
+
+	//Validacion si el usuario quiere ingresar la pagina datosRecibidos.php sin enviar datos formulario
+	if(!$_POST){
+
+		//header = paginaMostrada no hay datos en el $_POST
+		header('location:http://localhost/CursoPHP/formulario/formulario.php');
+	}
 
 	/*Accedemos al arreglo creado cuando usamos el metodo POST
 	 $_POST = arreglo del metodo post creada
 	*/
 	print_r($_POST); /*Array ( [nombre] => viktor [genero] => masculino [year] => 1985 [comentario] => comentario viktor [terminos] => true ) */
+	
 	/*Accedemos al contenido del arreglo*/
 	$nombre = $_POST['nombre'];
 	$genero = $_POST['genero'];
@@ -20,6 +29,7 @@
 		echo "dato ".$i."  valor<br>";
 	}
 	*/
+	
 	/*Mostrando valor*/
 	echo "<h2> Tu nombre es ".$nombre." de genero ".$genero." naciste en ".$year."</h2>";
 	echo "<h3>Tu comentario fue : ".$comentario." </h3>";
